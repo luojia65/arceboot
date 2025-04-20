0e744bb6847f7b472b4ae89a6907b9055f1c7258
Zhouqi Jiang <luojia@hust.edu.cn>
2025-04-20 11:05:57 +0800

    feat(main): read file from arceos axfs
    Modify readme to mount file system on QEMU
    
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ArceBoot
+
 Reuse [ArceOS](https://github.com/arceos-org/arceos) components to build a cross-platform operating system bootloader
+
+## Run ArceBoot
+
+Use ArceOS Makefile:
+
+```shell
+make A=<path/to/arceboot> ARCH=riscv64 BLK=y run
+```
--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,33 @@
 #[cfg(feature = "axstd")]
 extern crate axstd as std;
 
-use axdriver::AxDeviceContainer;
-use axdriver_block::ramdisk::RamDisk;
+// use axdriver::AxDeviceContainer;
+// use axdriver_block::ramdisk::RamDisk;
 use axfs::api as fs;
 use fs::File;
 use object::{Object, ObjectSection};
 
-fn scan_disk() -> std::io::Result<RamDisk> {
-    // By now, we provide image through command line arguments.
-    // In the future we should scan bootload media and choose which disk to boot from.
-    let args = std::env::args();
-    if args.len() < 2 {
-        eprintln!("Please provide disk from program argument.");
-        std::process::exit(1);
-    }
-    let path = args.skip(1).next().unwrap();
+// fn scan_disk() -> std::io::Result<RamDisk> {
+//     // By now, we provide image through command line arguments.
+//     // In the future we should scan bootload media and choose which disk to boot from.
+//     let args = std::env::args();
+//     if args.len() < 2 {
+//         eprintln!("Please provide disk from program argument.");
+//         std::process::exit(1);
+//     }
+//     let path = args.skip(1).next().unwrap();
 
-    println!("Loading disk image from {:?} ...", path);
-    let data = std::fs::read(path)?;
-    println!("size = {} bytes", data.len());
-    Ok(RamDisk::from(&data))
-}
+//     println!("Loading disk image from {:?} ...", path);
+//     let data = std::fs::read(path)?;
+//     println!("size = {} bytes", data.len());
+//     Ok(RamDisk::from(&data))
+// }
 
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
     println!("Opening arceboot.img...");
-    let disk = scan_disk().expect("load disk image");
-    axfs::init_filesystems(AxDeviceContainer::from_one(disk));
+    // let disk = scan_disk().expect("load disk image");
+    // axfs::init_filesystems(AxDeviceContainer::from_one(disk));
     let fname = "/EFI/BOOT/BOOTRISCV64.EFI";
     println!("Reading fname: {}", fname);
     let file = File::options().read(true).write(true).open(fname).unwrap();
